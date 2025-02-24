e572f847b97a28e432a084afa07b699e14707602
Anish Mainali <130769914+anishmendes@users.noreply.github.com>
2025-02-24 17:28:05 +0545

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
 import React from "react";
-import Hero from "./components/Hero/Hero";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Footer from "./components/Footer/Footer";
+import SignUp from "./components/Auth/SignUp"; // Create this component if not already present
 
-
-import Footer from "./components/Footer/Footer.jsx";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-
 const App = () => {
-  const [orderPopup, setOrderPopup] = React.useState(false);
-
-  const handleOrderPopup = () => {
-    setOrderPopup(!orderPopup);
-  };
-
   React.useEffect(() => {
     AOS.init({
       offset: 100,
@@ -26,14 +21,19 @@ const App = () => {
   }, []);
 
   return (
-    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
-      <Navbar handleOrderPopup={handleOrderPopup} />
-      <Hero handleOrderPopup={handleOrderPopup} />
-      
-
-      <Footer />
-    
-    </div>
+    <Router>
+      <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/about" element={<div>About Page</div>} />
+          <Route path="/contact" element={<div>Contact Page</div>} />
+          <Route path="*" element={<div>404 - Page Not Found</div>} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
